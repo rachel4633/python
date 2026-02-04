e00ee42a49adb15938d6c47317e735f21c1a7216
Rakel <nkiroterakel46@gmail.com>
2026-02-04 13:23:28 +0300

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# Python Data types
+# String - It is a series of characters that are inside of quotes
+name = "Rakel Kerubo"
+print(name)
+print(type(name))
+
+# Numertic
+# Int- This indicates a whole number in pyhton. For an integer, we don't use quotes
+age = 39
+print("The age of a person is: ", age)
+print(type(age))
+
+#float - This is a number with a decimal
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
+#Research and come up with a example of  complex data type
